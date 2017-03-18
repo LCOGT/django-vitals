8da216193fb1735989d70739ae4956c119accd2c
Austin Riba <ariba@lco.global>
2017-03-17 17:17:25 -0700

    update setup.py for pypi upload

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-vitals',
-    version='0.0.1',
+    version='1.0.0',
     packages=find_packages(),
     include_package_data=True,
-    license='GPLv2',
+    license='GPLv3',
     description='A django app that provides health check endpoints for vital services.',
     url='https://github.com/LCOGT/django-vitals',
     author='Austin Riba',
@@ -18,13 +18,12 @@ setup(
         'Environment :: Web Environment',
         'Framework :: Django',
         'Intended Audience :: Developers',
-        'Operating System :: OS Independant',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Topic :: Internet :: WWW/HTTP',
-        'TOPIC :: Internet :: WWW/HTTP :: Dynamic Content',
     ],
     install_requires=[
         'requests',
